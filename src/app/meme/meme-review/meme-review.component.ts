1fd5b65eed69fe1e0f01824f5e0774e88b4feda8
Coding Unicorn <coding-unicorn@zombie-unicorn.pl>
2020-04-09 19:18:43 +0200

    Add simple pagination

--- src/app/meme/meme-review/meme-review.component.ts
+++ src/app/meme/meme-review/meme-review.component.ts
@@ -9,16 +9,55 @@ import {HttpClient} from '@angular/common/http';
 export class MemeReviewComponent implements OnInit {
 
   private memesData;
+  private page: number;
+  private pagesCount: number;
+  private readonly mediaLimit: number;
+
+  memesUrl = '/api/memes/page';
 
   constructor(private readonly http: HttpClient) {
+    this.mediaLimit = 10;
+    this.page = 1;
   }
 
   async ngOnInit(): Promise<void> {
-    const url = '/api/memes/get';
-    this.memesData = await this.http.get(url).toPromise();
+    const params = {
+      number: '1',
+      limit: this.mediaLimit.toString(),
+    };
+    this.memesData = await this.http.get(this.memesUrl, {params}).toPromise();
+
+    const countUrl = '/api/memes/count';
+    const mediaCount: any = await this.http.get(countUrl).toPromise();
+    this.pagesCount = Math.ceil(mediaCount / this.mediaLimit);
+  }
+
+  async nextPage() {
+    if (this.page >= this.pagesCount) {
+      return;
+    }
+
+    const params = {
+      number: (++this.page).toString(),
+      limit: this.mediaLimit.toString(),
+    };
+    this.memesData = await this.http.get(this.memesUrl, {params}).toPromise();
+  }
+
+  async previousPage() {
+    if (this.page <= 1) {
+      return;
+    }
+
+    const params = {
+      number: (--this.page).toString(),
+      limit: this.mediaLimit.toString(),
+    };
+    this.memesData = await this.http.get(this.memesUrl, {params}).toPromise();
   }
 
   get MemesData() {
     return this.memesData;
   }
+
 }
