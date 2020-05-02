b0fb091839122b520c1f789a55f9fb8cb704168b
Coding Unicorn <coding-unicorn@zombie-unicorn.pl>
2020-05-02 21:40:20 +0200

    Allow adding comments to the meme

--- src/app/meme/meme-view/meme-view.component.ts
+++ src/app/meme/meme-view/meme-view.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
 import {AuthService} from '../../auth/services/auth.service';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -11,22 +12,48 @@ import {AuthService} from '../../auth/services/auth.service';
 export class MemeViewComponent implements OnInit {
 
   public meme: any;
+  public comments: any;
+
+  public commentForm: FormGroup;
 
   constructor(private readonly http: HttpClient,
               private readonly route: ActivatedRoute,
-              private readonly authService: AuthService) {
+              private readonly authService: AuthService,
+              private readonly formBuilder: FormBuilder) {
+    this.commentForm = this.formBuilder.group({
+      commentText: [],
+    });
   }
 
   async ngOnInit(): Promise<void> {
     const id = this.route.snapshot.paramMap.get('id');
     this.meme = await this.http.get(`/api/memes/get/${id}`).toPromise();
+
+    this.comments = await this.getComments();
   }
 
   async rateUp() {
     const headers = {
       Authorization: `Bearer ${this.authService.CurrentUser.token}`
     };
-    await this.http.put(`/api/memes/rateplus/${this.meme.id}`, {},{ headers }).toPromise();
+    await this.http.put(`/api/memes/rateplus/${this.meme.id}`, {}, {headers}).toPromise();
     window.location.reload();
+  }
+
+  async addComment() {
+    const headers = {
+      Authorization: `Bearer ${this.authService.CurrentUser.token}`
+    };
+    const body = {
+      Content: this.commentForm.value.commentText,
+      memeId: this.meme.id
+    };
+    await this.http.post(`api/comments`, body, {headers}).toPromise();
+    await this.getComments();
+    window.location.reload();
+  }
+
+  private async getComments() {
+    this.comments = await this.http.get(`api/comments/getbyid?id=${this.meme.id}`).toPromise();
   }
 }
